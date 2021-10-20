323a6dbbcadceda3a3173f32c8968ce82ed8fecf
sumon6638-sm <mdsumoncse19@gmail.com>
2021-10-19 23:04:52 -0400

    finallySubmitvai_firebaseconfig_dynamic

--- src/pages/Logins/Firebase/firebase.config.js
+++ src/pages/Logins/Firebase/firebase.config.js
@@ -1,21 +1,10 @@
-// console.log(process.env);
-
 const firebaseConfig = {
-    apiKey: "AIzaSyAH1lDAI-kQZCFGRy7DZZteUHoEVqTyTOU",
-    authDomain: "medical-planet-sumon6638.firebaseapp.com",
-    projectId: "medical-planet-sumon6638",
-    storageBucket: "medical-planet-sumon6638.appspot.com",
-    messagingSenderId: "198978721962",
-    appId: "1:198978721962:web:be283ea15994bd721598d3"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
-
-// const firebaseConfig = {
-//     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-//     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-//     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-//     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-//     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-//     appId: process.env.REACT_APP_FIREBASE_APP_ID,
-// };
 
 export default firebaseConfig;
